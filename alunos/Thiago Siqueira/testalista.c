d729a452b98bea9464482f01e3989c3ac25660ab
Thiago Siqueira <thiago.siqueira@rnp.br>
2021-02-11 19:26:20 -0300

    Criação da pasta e exercícios feitos

---
+++ alunos/Thiago Siqueira/testalista.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include "misc.h"
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+    int escolha;
+    int posicao;
+    
+    printf("Escolha a Operação Desejada:\n1 - Gerar Lista\n2 - Buscar\n");
+    scanf("%d", &escolha);
+    
+    if(escolha == 1)
+        geralista(100);
+    else if(escolha == 2)
+    {
+        printf("Digite a Posição da Lista a ser Buscada:\n");
+        scanf("%d", &posicao);
+        
+        char *numero = buscar(posicao);
+        
+        printf("O nº Presente na Posição %d %s %s", posicao, "É:", numero);
+    }
+        	
+	return 0;
+}
